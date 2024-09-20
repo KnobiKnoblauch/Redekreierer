3143386322c46e75e6834e4a8b4ab300ba04ab21
TayoZz <153221513+TayoZz@users.noreply.github.com>
2024-09-20 15:12:01 +0200

    update frontend.py
    not working

--- chatbot/frontend.py
+++ chatbot/frontend.py
@@ -1,18 +1,54 @@
 import streamlit as st
 import time
+#import chatgpt_benutzen
 
-st.title("_Der Redekreirer_ :blue[von Knobi]")
-st.subheader("Lalalalaalalala", divider="gray")
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
 
-    with st.status("Downloading data..."):
-    st.write("Searching for data...")
-    time.sleep(2)
-    st.write("Found URL.")
-    time.sleep(1)
-    st.write("Downloading data...")
-    time.sleep(1)
+
+# funktioniert noch nicht
+
+
+
+speak_type = {
+    "topic" : "",
+    "goal" : "",
+    "addresant" : "",
+    "speaker" : ""
+
+}
+
+
+def setup():
+    st.title("_Der Redekreirer_ :blue[von Knobi]")
+    st.subheader("Lalalalaalalala", divider="gray")
+    input_loop()
+
+def input_loop():
+    speak_type["topic"] = get_topic()
+    speak_type["goal"] = get_goal()
+    speak_type["addresant"] = get_addresant()
+    speak_type["speaker"] = get_speaker()
+
+def get_topic():
+    prompt = st.chat_input("What is your speech about?")
+    if prompt:
+         return prompt
+
+def get_goal():
+    prompt = st.chat_input("What are you trying to achieve with your speech?")
+    if prompt:
+         return prompt
+
+def get_addresant():
+    prompt = st.chat_input("Who listens to your speech?")
+    if prompt:
+         return prompt
+
+def get_speaker():
+    prompt = st.chat_input("Who is performing your speech?")
+    if prompt:
+         return prompt
+
+setup()
+
 
 st.button("Rerun")
