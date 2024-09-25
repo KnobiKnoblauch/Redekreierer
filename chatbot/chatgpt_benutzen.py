a9c32d1dbac23d5158c9c33a86dec0785a348465
KnobiKnoblauch <knobi_knoblauch@icloud.com>
2024-09-25 12:05:54 +0200

    modify: requirements.txt

--- chatbot/chatgpt_benutzen.py
+++ chatbot/chatgpt_benutzen.py
@@ -1,16 +1,14 @@
-
+import os
 from openai import OpenAI
+from dotenv import load_dotenv
 
 from prompt_creator import PromptCreationService
-from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
 api_key = os.getenv("API_KEY")
 
 client = OpenAI(api_key=api_key)
-
 
 prompt_creation_service = PromptCreationService()
 
